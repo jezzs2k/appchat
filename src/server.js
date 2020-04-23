6d6d6c9a6eb373b5c71982a1919569228f2405db
HIEU VU THANH <jezzs2k@gmail.com>
2020-04-23 16:38:41 +0700

    setup server

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+app.listen(PORT, () => {
+  console.log('Server on with port: ', PORT);
+});

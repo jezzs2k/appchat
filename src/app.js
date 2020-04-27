15b94a5b071bb6ab103d74d474d4e56aed44318c
HIEU VU THANH <jezzs2k@gmail.com>
2020-04-27 14:18:29 +0700

    add app server

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// CREATE ROUTER
+const authRoute = require('./routes/auth.route');
+const userRoute = require('./routes/user.route');
+const contactRoute = require('./routes/contact.route');
+const conversationRoute = require('./routes/Conversation.route');
+const messengerRoute = require('./routes/messenger.route');
+
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+app.use('/api/contacts', contactRoute);
+app.use('/api/conversations', conversationRoute);
+app.use('/api/messengers', messengerRoute);
+
+module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,28 +1,9 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const app = require('./app');
 
-const app = express();
 const PORT = process.env.PORT || 8080;
 const connectDB = require('./config/db');
 
 connectDB();
-
-app.use(bodyParser.json());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// CREATE ROUTER
-const authRoute = require('./routes/auth.route');
-const userRoute = require('./routes/user.route');
-const contactRoute = require('./routes/contact.route');
-const conversationRoute = require('./routes/Conversation.route');
-const messengerRoute = require('./routes/messenger.route');
-
-app.use('/api/auth', authRoute);
-app.use('/api/users', userRoute);
-app.use('/api/contacts', contactRoute);
-app.use('/api/conversations', conversationRoute);
-app.use('/api/messengers', messengerRoute);
 
 app.listen(PORT, () => {
   console.log('Server on with port: ', PORT);
